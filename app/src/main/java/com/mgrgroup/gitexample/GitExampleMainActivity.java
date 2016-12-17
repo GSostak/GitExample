9c4f7146d858d35de42a440fdfadd01f62ab2ef3
George Sostak <gs@solutions100.com>
2016-12-17 17:35:19 +0100

    Initial commit

---
+++ app/src/main/java/com/mgrgroup/gitexample/GitExampleMainActivity.java
@@ -0,0 +1,13 @@
+package com.mgrgroup.gitexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GitExampleMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_example_main);
+    }
+}
